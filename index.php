39ee09929f3107618a82fad3fb6c6b530a2bafb1
mia starr dolan <miasdolan@gmail.com>
2014-11-04 12:24:56 -0800

    video 17 and 18

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
+<?php
+    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "/view/navigation.php");
+    require_once(__DIR__ . "/controller/create-db.php");
+    require_once(__DIR__ . "/view/footer.php");
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            require_once(__DIR__ . "/controller/create-db.php");
-        ?>
-     </body
-</html>
+    ?>
+
+
  
+
+
+
+
+
