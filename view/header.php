b4dbc2b25abd00ed3b5450ac839ebc1385131940
mia starr dolan <miasdolan@gmail.com>
2014-11-12 11:53:25 -0800

    tuesdays work

--- view/header.php
+++ view/header.php
@@ -6,7 +6,10 @@
 
 <html>
     <head>
+        
         <link rel="stylesheet" type="text/css" href="main.css">
+        <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+         
         <meta charset="UTF-8">
         <title></title>
     </head>
