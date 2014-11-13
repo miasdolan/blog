29c2d64dab7b8d0d086a6b78a152ac59682712d7
mia starr dolan <miasdolan@gmail.com>
2014-11-12 23:16:11 -0800

    blog 23 Establishing A Database Connection

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,6 +1,12 @@
 <?php
+    require_once(__DIR__ . "/../model/database.php");
+
+   $connection = new mysqli($host, $username, $password, $database);
+    
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
     echo "<p>Title: $title </p>";
     echo "<p>Post: $post </p>";
+    
+    $connection->close();
